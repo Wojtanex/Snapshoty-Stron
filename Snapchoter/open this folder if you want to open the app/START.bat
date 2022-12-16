2eba8449601cbed83908ec6016d9c5cf57327be7
Wojtanex <76729472+Wojtanex@users.noreply.github.com>
2022-12-16 19:26:52 +0100

    Update START.bat

--- Snapchoter/open this folder if you want to open the app/START.bat
+++ Snapchoter/open this folder if you want to open the app/START.bat
@@ -11,3 +11,5 @@ if %szukaj% == youtube.com start other_files\youtube.bat
 if %szukaj% == youtube start other_files\youtube.bat
 if %szukaj% == wikipedia.com start other_files\wikipedia.bat
 if %szukaj% == wikipedia start other_files\wikipedia.bat
+if %szukaj% == facebook.com start other_files\facebook.bat
+if %szukaj% == facebook start other_files\facebook.bat
